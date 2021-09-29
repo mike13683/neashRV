31ab210aaef1cbfa497652004e4cf2a1c11b07b0
mike owens <mike@dragoninfotech.com>
2021-09-29 16:16:35 +0100

    first commit

---
+++ assets/js/source.js
@@ -0,0 +1,16 @@
+
+
+
+$(document).ready(function(){
+
+
+
+    $(".owl-carousel").owlCarousel({
+        items:1,
+        navContainer: '.customNav',
+        nav:true,
+        navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+        dots: false
+    });
+
+});
